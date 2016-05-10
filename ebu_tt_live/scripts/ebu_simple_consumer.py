8f7fcf577af2855e889263d8717b96bb59f157ad
Zoltan Kozma <Zoltan.Kozma.ext@bbc.co.uk>
2016-05-10 13:24:00 +0100

    removing garbage code

--- ebu_tt_live/scripts/ebu_simple_consumer.py
+++ ebu_tt_live/scripts/ebu_simple_consumer.py
@@ -1,13 +1,7 @@
 import logging
 from argparse import ArgumentParser
-from .common import create_loggers, parse_config
-from ebu_tt_live.consumers import ConsumerFactory
+from .common import create_loggers
 from ebu_tt_live.clocks.local import LocalMachineClock
-from ebu_tt_live import bindings
-from zope.interface import registry, adapter
-from twisted.python import components
-from itertools import cycle
-from time import sleep
 
 
 log = logging.getLogger('ebu_simple_consumer')
@@ -24,15 +18,3 @@ def main():
     log.info('This is a Simple Consumer example')
 
     clock = LocalMachineClock()
-
-    def gen_time():
-        while True:
-            yield clock.get_full_clock_time()
-
-    bla = 0
-    for item in gen_time():
-        print item
-        bla += 1
-        sleep(0.2)
-        if bla > 20:
-            break
