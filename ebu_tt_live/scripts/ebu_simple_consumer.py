e3ff7ad01c54bd6253b16f96ffb3d72326920fb9
Zoltan Kozma <Zoltan.Kozma.ext@bbc.co.uk>
2016-05-10 17:44:32 +0100

    consumer hardcoded basics

--- ebu_tt_live/scripts/ebu_simple_consumer.py
+++ ebu_tt_live/scripts/ebu_simple_consumer.py
@@ -2,6 +2,8 @@ import logging
 from argparse import ArgumentParser
 from .common import create_loggers
 from ebu_tt_live.clocks.local import LocalMachineClock
+from autobahn.twisted.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
+from twisted.internet import reactor
 
 
 log = logging.getLogger('ebu_simple_consumer')
@@ -12,9 +14,24 @@ parser.add_argument('-c', '--config', dest='config', metavar='CONFIG')
 items = [1,2,3,4,5,6,7]
 
 
+class ClientNodeProtocol(WebSocketClientProtocol):
+
+    def onOpen(self):
+        self.sendMessage('Hello')
+
+    def onMessage(self, payload, isBinary):
+
+        log.info(payload)
+
+
 def main():
     args = parser.parse_args()
     create_loggers()
     log.info('This is a Simple Consumer example')
 
-    clock = LocalMachineClock()
+    factory = WebSocketClientFactory('ws://localhost:9000')
+    factory.protocol = ClientNodeProtocol
+
+    connectWS(factory=factory)
+
+    reactor.run()
