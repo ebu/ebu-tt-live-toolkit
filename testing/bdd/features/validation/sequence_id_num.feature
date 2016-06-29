9bb2f02b9c14f572224e445277f736b0096b501c
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-06-29 09:54:05 +0200

    new tests on tt's sequence attributes

--- testing/bdd/features/validation/sequence_id_num.feature
+++ testing/bdd/features/validation/sequence_id_num.feature
@@ -11,3 +11,17 @@ Feature: Sequence ID and Sequence Number
         | xml_file            | seq_id   | seq_n |
         | sequence_id_num.xml |          | 5     |
         | sequence_id_num.xml | testSeq1 | a     |
+        | sequence_id_num.xml | testSeq1 | -5    |
+
+    Scenario: Valid Sequence head attributes
+        Given a xml file <xml_file>
+        And it has sequence identifier <seq_id>
+        And it has sequence number <seq_n>
+        Then document is valid
+
+        Examples:
+        | xml_file            | seq_id   | seq_n     |
+        | sequence_id_num.xml | testSeq1 | 5         |
+        | sequence_id_num.xml | a        | 10        |
+        | sequence_id_num.xml | testSeq1 | 999999999 |
+
---
+++ testing/bdd/test_sequence_attributes_validation.py
@@ -0,0 +1,31 @@
+from ebu_tt_live.documents import EBUTT3Document
+from pyxb import SimpleTypeValueError
+from pytest_bdd import given, then, scenarios
+import pytest
+
+
+scenarios('features/validation/sequence_id_num.feature')
+
+
+@given('it has sequence identifier <seq_id>')
+def sequence_id(seq_id):
+    return seq_id
+
+
+@given('it has sequence number <seq_n>')
+def sequence_number(seq_n):
+    return seq_n
+
+
+@then('document is invalid')
+def invalid_doc(template_file, sequence_id, sequence_number):
+    xml = template_file.render(sequence_id=sequence_id, sequence_num=sequence_number)
+    with pytest.raises(SimpleTypeValueError):
+        EBUTT3Document.create_from_xml(xml)
+
+
+@then('document is valid')
+def valid_doc(template_file, sequence_id, sequence_number):
+    xml = template_file.render(sequence_id=sequence_id, sequence_num=sequence_number)
+    document = EBUTT3Document.create_from_xml(xml)
+    assert isinstance(document, EBUTT3Document)
