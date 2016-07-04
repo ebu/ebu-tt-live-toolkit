858177512f602b842f934324cd71e6f8612f6da1
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-07-04 15:12:12 +0200

    referenceClockIdentifier tests done

---
+++ testing/bdd/test_referenceClockIdentifier.py
@@ -0,0 +1,18 @@
+from pytest_bdd import scenarios, given
+
+scenarios('features/validation/referenceClockIdentifier_constraints.feature')
+
+
+@given('it has timeBase <time_base>')
+def given_time_base(time_base, template_dict):
+    template_dict['time_base'] = time_base
+
+
+@given('it has clock mode <clock_mode>')
+def given_body_begin(clock_mode, template_dict):
+    template_dict['clock_mode'] = clock_mode
+
+
+@given('it has reference clock identifier <ref_clock_id>')
+def given_body_end(ref_clock_id, template_dict):
+    template_dict['ref_clock_id'] = ref_clock_id
