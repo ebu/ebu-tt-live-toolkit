5e8718cf104f09c9659c47acccb1adc9fe4fac8d
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-07-11 15:16:28 +0200

    more unit testign

--- ebu_tt_live/carriage/test/test_filesystem.py
+++ ebu_tt_live/carriage/test/test_filesystem.py
@@ -1,4 +1,9 @@
 from unittest import TestCase
+try:
+    from unittest.mock import patch, MagicMock
+except ImportError:
+    from mock import patch, MagicMock
+
 from ebu_tt_live.carriage import FileSystemCarriageImpl
 import os
 import tempfile
@@ -16,3 +21,13 @@ class TestFileSystemCarriageImpl(TestCase):
     def test_instantiation(self):
         fs_carriage = FileSystemCarriageImpl(os.path.abspath(self.test_dir))
         self.assertIsInstance(fs_carriage, FileSystemCarriageImpl)
+
+    @patch('ebu_tt_live.node.SimpleProducer')
+    def test_resume_producing(self, simpleProducer):
+        fs_carriage = FileSystemCarriageImpl(os.path.abspath(self.test_dir))
+        simpleProducer.process_document = MagicMock(return_value=False)
+        fs_carriage.register(simpleProducer)
+        fs_carriage.resume_producing()
+        assert simpleProducer.process_document.called
+        manifest_path = os.path.join(os.path.abspath(self.test_dir), 'manifest.txt')
+        assert os.path.exists(manifest_path)
