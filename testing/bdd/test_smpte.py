4473ba9d24808dfc7cc1c9121386161e5ba58957
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-07-12 14:15:29 +0200

    extend smpte tests

--- testing/bdd/test_smpte.py
+++ testing/bdd/test_smpte.py
@@ -11,3 +11,18 @@ def given_time_base(time_base, template_dict):
 @given('it has frameRate <frame_rate>')
 def given_frame_rate(frame_rate, template_dict):
     template_dict['frame_rate'] = frame_rate
+
+
+@given('it has frameRateMultiplier <frame_rate_multiplier>')
+def given_frame_rate_multiplier(frame_rate_multiplier, template_dict):
+    template_dict['frame_rate_multiplier'] = frame_rate_multiplier
+
+
+@given('it has dropMode <drop_mode>')
+def given_drop_mode(drop_mode, template_dict):
+    template_dict['drop_mode'] = drop_mode
+
+
+@given('it has markerMode <marker_mode>')
+def given_marker_mode(marker_mode, template_dict):
+    template_dict['marker_mode'] = marker_mode
