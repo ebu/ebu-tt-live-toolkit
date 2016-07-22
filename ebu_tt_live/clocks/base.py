b564415e9ce4d293f078b57c690d25da94396ac8
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-07-22 09:33:29 +0200

    documentation and better naming

--- ebu_tt_live/clocks/base.py
+++ ebu_tt_live/clocks/base.py
@@ -12,19 +12,19 @@ class Clock(object):
     _time_base = None
     # Implemented to be used with the manifest file, allows us to save a fix
     # time that we can read back later.
-    _saved_time = None
-    _use_saved_time = False
+    _fixed_time = None
+    _use_fixed_time = False
 
     def get_time(self):
         """
         :return: datetime.timedelta object
         """
-        if self._use_saved_time:
-            return self._saved_time
+        if self._use_fixed_time:
+            return self._fixed_time
         else:
-            return self.current_time()
+            return self.get_real_clock_time()
 
-    def current_time(self):
+    def get_real_clock_time(self):
         """
         Implemented in descendant classes
         :return: datetime.timedelta object
@@ -43,17 +43,37 @@ class Clock(object):
     def time_base(self):
         return self._time_base
 
-    def save_time(self, value):
+    def set_fixed_time(self, value):
+        """
+        Registers a time in the Clock object. If fixed-time mode is active, e.g. `set_fixed_time_mode(true)`
+        was called on self, then every call to `get_time()` will return the registered time. The parameter
+        `value` must contain the time as a datetime.timedelta. If it is not a datetime.timedelta, a `TypeError`
+        exception will be raised.
+        :param value: the time to register as the fixed time of the Clock. Must of type datetime.timedelta
+        """
         if not isinstance(value, timedelta):
             raise TypeError
-        else:
-            self._saved_time = value
+        self._fixed_time = value
 
-    def saved_time_mode(self):
-        self.use_saved_time = True
+    def set_fixed_time_mode(self, value):
+        """
+        Used to activate or deactivate the fixed-time mode. If `value` is True the clock will be set in
+        fixed-time mode, while this mode is active any call to `get_time()` will return the time registered
+        with `set_fixed_time` as the fixed time of clock. If `value` is False, the fixed-time mode is
+        deactivated and the real time of the clock is returned when `get_time()` is called. If `value` is not of
+        type `bool` a TypeError exception will be raised.
+        :param value: a bool. True to activate fixed-time mode, False to deactivate fixed-time mode.
+        """
+        if not isinstance(value, bool):
+            raise TypeError
+        self.use_saved_time = value
 
-    def is_saved_time_mode(self):
-        return self._use_saved_time
+    def is_fixed_time_mode(self):
+        """
+        Returns the current status of the fixed-time mode.
+        :return: True if fixed-time mode is currently active, False otherwise.
+        """
+        return self._use_fixed_time
 
     def start(self):
         pass
--- ebu_tt_live/clocks/local.py
+++ ebu_tt_live/clocks/local.py
@@ -6,6 +6,6 @@ class LocalMachineClock(Clock):
 
     _time_base = 'clock'
 
-    def current_time(self):
+    def get_real_clock_time(self):
         now = datetime.now().time()
         return timedelta(hours=now.hour, minutes=now.minute, seconds=now.second, microseconds=now.microsecond)
