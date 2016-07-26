527f265edc8cf9d64644dcb476514733775c2dcd
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-07-26 11:32:36 +0200

    keep only the producer part here

--- ebu_tt_live/carriage/filesystem.py
+++ ebu_tt_live/carriage/filesystem.py
@@ -1,8 +1,5 @@
-from .base import ProducerCarriageImpl, ConsumerCarriageImpl
-from ebu_tt_live.documents import EBUTT3Document
-from ebu_tt_live.bindings import CreateFromDocument
-from ebu_tt_live.strings import ERR_DECODING_XML_FAILED
-from ebu_tt_live.errors import XMLParsingFailed, EndOfData
+from .base import ProducerCarriageImpl
+from ebu_tt_live.errors import EndOfData
 import logging
 import os
 import datetime
@@ -74,58 +71,3 @@ class FilesystemProducerImpl(ProducerCarriageImpl):
         self._manifest_file = open(self._manifest_path, 'w')
         self._manifest_file.write(self._manifest_content)
         self._manifest_file.close()
-
-
-class FilesystemConsumerImpl(ConsumerCarriageImpl):
-    """
-    This class is where the logic for availability times should be implemented. This allows us to implement different
-    interpretation of availability times by simply adding new CarriageMechanism, the reading of the files itself being
-    done in ebutt_live.carriage.FilesystemReader file.
-    """
-
-    def on_new_data(self, data):
-        document = None
-        availability_time, xml_content = data
-        try:
-            document = EBUTT3Document.create_from_raw_binding(CreateFromDocument(xml_content))
-        except:
-            log.exception(ERR_DECODING_XML_FAILED)
-            raise XMLParsingFailed(ERR_DECODING_XML_FAILED)
-
-        if document:
-            self._node.process_document(document)
-
-
-class FilesystemReader(object):
-    """
-    This class is responsible for reading the manifest file and sending the corresponding
-    availability times and xml file's content to its _custom_consumer.
-    """
-    _dirpath = None
-    _manifest_lines_iter = None
-    _custom_consumer = None
-    _manifest_time_format = None
-
-    def __init__(self, dirpath, custom_consumer):
-        self._dirpath = dirpath
-        self._custom_consumer = custom_consumer
-        manifest_path = os.path.join(dirpath, 'manifest.txt')
-        with open(manifest_path, 'r') as manifest:
-            self._manifest_lines_iter = iter(manifest.readlines())
-
-    def resume_reading(self):
-        if not self._manifest_time_format:
-            self._manifest_time_format = MANIFEST_TIME_CLOCK_FORMAT
-        manifest_line = None
-        try:
-            manifest_line = self._manifest_lines_iter.next()
-        except StopIteration:
-            raise EndOfData
-        availability_time_str, xml_file_name = manifest_line.rstrip().split(',')
-        availability_time = datetime.datetime.strptime(availability_time_str, MANIFEST_TIME_CLOCK_FORMAT).time()
-        xml_file_path = os.path.join(self._dirpath, xml_file_name)
-        xml_content = None
-        with open(xml_file_path, 'r') as xml_file:
-            xml_content = xml_file.read()
-        data = [availability_time, xml_content]
-        self._custom_consumer.on_new_data(data)
