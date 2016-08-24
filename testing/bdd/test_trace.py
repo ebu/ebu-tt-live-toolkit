098d9d6f34fd632cc1d0241d539eb57189cab508
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2016-08-24 14:22:08 +0200

    update conformance document + check source_id attribute of trace is optionnal + reference R numbers in tests

--- testing/bdd/test_trace.py
+++ testing/bdd/test_trace.py
@@ -12,3 +12,8 @@ def when_trace_action_attribute(action, template_dict):
 @when('trace element has generatedBy attribute <generated_by>')
 def when_trace_generatedBy_attribute(generated_by, template_dict):
     template_dict["generated_by"] = generated_by
+
+
+@when('trace element has sourceId attribute <source_id>')
+def when_trace_sourceId_attribute(source_id, template_dict):
+    template_dict["source_id"] = source_id
