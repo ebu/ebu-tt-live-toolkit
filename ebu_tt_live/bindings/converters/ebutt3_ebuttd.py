2685ad89c89d4d9e17d197665c11a060f17fd879
Zoltan Kozma <zoltan.kozma.ext@bbc.co.uk>
2016-08-25 11:22:37 +0100

    #170 getting stuff moving

--- ebu_tt_live/bindings/converters/ebutt3_ebuttd.py
+++ ebu_tt_live/bindings/converters/ebutt3_ebuttd.py
@@ -1,5 +1,11 @@
-from ebu_tt_live.bindings import tt_type, d_tt_type
+from ebu_tt_live.bindings import tt_type, d_tt_type, body_type, d_body_type, div_type, d_div_type, \
+    p_type, d_p_type, span_type, d_span_type, br_type, d_br_type
 
 conversion_mappings = {
-    tt_type: d_tt_type
+    tt_type: d_tt_type,
+    body_type: d_body_type,
+    div_type: d_div_type,
+    p_type: d_p_type,
+    span_type: d_span_type,
+    br_type: d_br_type
 }
