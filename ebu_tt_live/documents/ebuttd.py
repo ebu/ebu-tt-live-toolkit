a75551f97c4c5c51cc8bbb3ff19b7c1cba097d23
Zoltan Kozma <Zoltan.Kozma.ext@bbc.co.uk>
2016-08-25 17:07:05 +0100

    #174

--- ebu_tt_live/documents/ebuttd.py
+++ ebu_tt_live/documents/ebuttd.py
@@ -21,7 +21,7 @@ class EBUTTDDocument(SubtitleDocument):
     @classmethod
     def create_from_xml(cls, xml_in):
         document = EBUTTDDocument()
-        document._ebuttd_content = bindings.ttd.CreateFromXML()
+        document._ebuttd_content = bindings.ttd.CreateFromXML(xml_in)
 
     @classmethod
     def create_from_raw_binding(cls, binding_in):
@@ -31,5 +31,5 @@ class EBUTTDDocument(SubtitleDocument):
 
     @classmethod
     def create_from_ebutt3(cls, ebutt3_in):
-        ebuttd_bindings = EBUTT3EBUTTDConverter.convert_element(ebutt3_in)
+        ebuttd_bindings = EBUTT3EBUTTDConverter.convert_element(ebutt3_in.binding)
         return cls.create_from_raw_binding(ebuttd_bindings)
