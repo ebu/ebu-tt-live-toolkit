14b74886ba6cd211d03b8a90813d210777587812
malikbeytrison <malik.beytrison52@gmail.com>
2016-10-14 10:26:42 +0200

    change sequence identifier and timing attributes of body

--- ebu_tt_live/node/delay.py
+++ ebu_tt_live/node/delay.py
@@ -1,14 +1,48 @@
+
 from .base import Node
+from datetime import timedelta
+from ebu_tt_live.bindings._ebuttdt import LimitedClockTimingType
+import time
+import random
+
 
 class FixedDelayNode(Node):
 
-	_reference_clock = None
-	_sequence = None
+    _reference_clock = None
+    _sequence = None
 
-	def __init__(self, node_id, carriage_impl, reference_clock):
-		super(FixedDelayNode, self).__init__(node_id, carriage_impl)
-		self._reference_clock = reference_clock
+    _document_queue = Queue()
 
-	def process_document(self, document):
+    def __init__(self, node_id, carriage_impl, reference_clock):
+        super(FixedDelayNode, self).__init__(node_id, carriage_impl)
+        self._reference_clock = reference_clock
 
-		pass
+        random_int = random.randint(0, 9999999999)
+        new_identifier = 'sequence_{0}'.format(str(random_int))
+        self._sequence = new_identifier
+
+    def process_document(self, document):
+
+        fixed_delay = LimitedClockTimingType(timedelta(seconds=2))
+
+        # change the sequence identifier
+        document.sequence_identifier = self._sequence
+
+        # TODO: add an ebuttm:trace element to the document metadata
+
+        # document is explicitly timed: modify the document
+        if document.binding.body.begin or document.binding.body.end:
+
+            document.binding.body.begin += fixed_delay
+            document.binding.body.end += fixed_delay
+            document.binding.body.dur += fixed_delay
+
+            # TODO: traverse all the elements on content and change their begin/end/dur attributes
+
+            self._carriage_impl.emit_document(document)
+
+        # document is implicitly timed: pause a while, re-emit later
+        else:
+
+            time.sleep(fixed_delay)
+            self._carriage_impl.emit_document(document)
