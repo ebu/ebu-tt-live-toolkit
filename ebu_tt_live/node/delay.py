49aad87f6b4849b82accb15f0a633731afed5bc0
malikbeytrison <malik.beytrison52@gmail.com>
2016-10-14 17:33:03 +0200

    small fixes with begin/end/dur times

--- ebu_tt_live/node/delay.py
+++ ebu_tt_live/node/delay.py
@@ -21,37 +21,41 @@ class FixedDelayNode(Node):
 
     def process_document(self, document):
 
-        fixed_delay = LimitedClockTimingType(timedelta(seconds=2))
-
         # change the sequence identifier
         document.sequence_identifier = self._sequence
 
         # TODO: add an ebuttm:trace element to the document metadata
+
+        fixed_delay_int = 2
 
         # document is explicitly timed: modify the document
         if document.binding.body.begin or document.binding.body.end:
 
             if document.time_base == 'clock':
 
+                fixed_delay = LimitedClockTimingType(timedelta(seconds=fixed_delay_int))
+
                 document.set_begin(
                     LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
 
                 document.set_end(
-                    LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+                    LimitedClockTimingType(document.binding.body.end.timedelta + fixed_delay.timedelta))
 
                 document.set_dur(
-                    LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+                    LimitedClockTimingType(document.binding.body.dur.timedelta + fixed_delay.timedelta))
 
             elif document.time_base == 'media':
 
+                fixed_delay = FullClockTimingType(timedelta(seconds=fixed_delay_int))
+
                 document.set_begin(
                     FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
 
                 document.set_end(
-                    FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+                    FullClockTimingType(document.binding.body.end.timedelta + fixed_delay.timedelta))
 
                 document.set_dur(
-                    FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+                    FullClockTimingType(document.binding.body.dur.timedelta + fixed_delay.timedelta))
 
             # TODO: traverse all the elements on content and change their begin/end/dur attributes
 
@@ -60,5 +64,5 @@ class FixedDelayNode(Node):
         # document is implicitly timed: pause a while, re-emit later
         else:
 
-            time.sleep(fixed_delay)
+            time.sleep(fixed_delay_int)
             self._carriage_impl.emit_document(document)
