4b0917f86892bf519d55507e7b3f7c785739161f
malikbeytrison <malik.beytrison52@gmail.com>
2016-10-14 11:44:30 +0200

    taking into account time_base for clock type

--- ebu_tt_live/node/delay.py
+++ ebu_tt_live/node/delay.py
@@ -1,7 +1,7 @@
 
 from .base import Node
 from datetime import timedelta
-from ebu_tt_live.bindings._ebuttdt import LimitedClockTimingType
+from ebu_tt_live.bindings._ebuttdt import LimitedClockTimingType, FullClockTimingType
 import time
 import random
 
@@ -10,8 +10,6 @@ class FixedDelayNode(Node):
 
     _reference_clock = None
     _sequence = None
-
-    _document_queue = Queue()
 
     def __init__(self, node_id, carriage_impl, reference_clock):
         super(FixedDelayNode, self).__init__(node_id, carriage_impl)
@@ -33,9 +31,27 @@ class FixedDelayNode(Node):
         # document is explicitly timed: modify the document
         if document.binding.body.begin or document.binding.body.end:
 
-            document.binding.body.begin += fixed_delay
-            document.binding.body.end += fixed_delay
-            document.binding.body.dur += fixed_delay
+            if document.time_base == 'clock':
+
+                document.set_begin(
+                    LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+
+                document.set_end(
+                    LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+
+                document.set_dur(
+                    LimitedClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+
+            elif document.time_base == 'media':
+
+                document.set_begin(
+                    FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+
+                document.set_end(
+                    FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
+
+                document.set_dur(
+                    FullClockTimingType(document.binding.body.begin.timedelta + fixed_delay.timedelta))
 
             # TODO: traverse all the elements on content and change their begin/end/dur attributes
 
