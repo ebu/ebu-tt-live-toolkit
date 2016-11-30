69178e0b59b878cd320abb0e578e1cd07eeb0ce5
malikbeytrison <malik.beytrison52@gmail.com>
2016-11-30 13:38:14 +0100

    unit tests for delay nodes

--- ebu_tt_live/node/test/test_delay_unit.py
+++ ebu_tt_live/node/test/test_delay_unit.py
@@ -1,9 +1,31 @@
 from unittest import TestCase
-from ebu_tt_live.node.delay import RetimingDelayNode
+from mock import MagicMock
+from ebu_tt_live.node.delay import RetimingDelayNode, BufferDelayNode
 
 
 class TestRetimingDelayNode(TestCase):
 
     def test_process_document(self):
 
-        pass
+        carriage = MagicMock()
+        reference_clock = MagicMock()
+        delay = 2
+        document_sequence = 'TestSequence'
+        node = RetimingDelayNode('delay_node', carriage, reference_clock, delay, document_sequence)
+        document = MagicMock()
+        node.process_document(document)
+        carriage.emit_document.assert_called_with(document)
+
+
+class TestBufferDelayNode(TestCase):
+
+    def test_process_document(self):
+
+        carriage = MagicMock()
+        reference_clock = MagicMock()
+        delay = 2
+        document_sequence = 'TestSequence'
+        node = BufferDelayNode('delay_node', carriage, reference_clock, delay, document_sequence)
+        document = MagicMock()
+        node.process_document(document)
+        carriage.emit_document.assert_called_with(document, delay=delay)
