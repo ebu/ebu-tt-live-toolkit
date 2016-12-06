e91eba64858a32d862e76706a5aa7862d2763bdb
Zoltan Kozma <zoltan.kozma.ext@bbc.co.uk>
2016-12-06 17:43:38 +0000

    this test leaves the reactor dirty....

--- ebu_tt_live/twisted/test/test_twisted_websocket.py
+++ ebu_tt_live/twisted/test/test_twisted_websocket.py
@@ -1,5 +1,7 @@
 
 from twisted.trial.unittest import TestCase
+from twisted.test import proto_helpers
+from twisted.internet import reactor
 from ebu_tt_live.twisted.websocket import BroadcastServerFactory, BroadcastServerProtocol
 from mock import MagicMock
 
@@ -10,10 +12,11 @@ class TestBroadcastServerFactory(TestCase):
         self.factory = BroadcastServerFactory()
         self.proto = BroadcastServerProtocol()
         self.proto.factory = self.factory
-        self.proto.log = MagicMock()
+        self.tr = proto_helpers.StringTransport()
+
 
     def tearDown(self):
         pass
 
     def test_connection_made(self):
-        pass
+        self.proto.makeConnection(self.tr)
