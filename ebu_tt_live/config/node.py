8ebc37aef6116444bee80f2ba8dc088f88970dae
Zoltan Kozma <zoltan.kozma.ext@bbc.co.uk>
2016-12-21 14:09:42 +0100

    normalize consumer and producer configurators

--- ebu_tt_live/config/node.py
+++ ebu_tt_live/config/node.py
@@ -16,23 +16,42 @@ class NodeBase(ConfigurableComponent):
     required_config.add_option('id', default='generic_node')
 
 
-class SimpleConsumer(NodeBase):
+class ProducerMixin(RequiredConfig):
+
     required_config = Namespace()
-    required_config.add_option('id', default='simple-consumer')
+    required_config.output = Namespace()
+    required_config.output.carriage = Namespace()
+    required_config.output.carriage.add_option(
+        'type', default='websocket', from_string_converter=producer_carriage_by_type
+    )
+    required_config.output.add_option('adapters')
+
+    _output = None
+
+    def _create_output(self, config=None):
+        self._output = self.config.output
+        self._output.carriage = self.config.output.carriage.type.configure_component(
+            config, self.config.output.carriage)
+
+        self._output.adapters = ProducerNodeCarriageAdapter.configure_component(
+            config=config,
+            local_config=self.config.output.adapters,
+            producer=self.component,
+            carriage=self._output.carriage.component
+        )
+
+
+class ConsumerMixin(RequiredConfig):
+
+    required_config = Namespace()
     required_config.input = Namespace()
     required_config.input.carriage = Namespace()
     required_config.input.carriage.add_option(
-        'type', default='websocket', from_string_converter=consumer_carriage_by_type)
+        'type', default='websocket', from_string_converter=consumer_carriage_by_type
+    )
     required_config.input.add_option('adapters')
-    required_config.clock = Namespace()
-    required_config.clock.add_option('type', default='auto', from_string_converter=clock_by_type)
 
     _input = None
-
-    def _create_component(self, config=None):
-        self.component = processing_node.SimpleConsumer(
-            node_id=self.config.id
-        )
 
     def _create_input(self, config=None):
         self._input = self.config.input
@@ -46,6 +65,22 @@ class SimpleConsumer(NodeBase):
             carriage=self._input.carriage.component
         )
 
+
+class SimpleConsumer(ConsumerMixin, NodeBase):
+    required_config = Namespace()
+    required_config.add_option('id', default='simple-consumer')
+    required_config.clock = Namespace()
+    required_config.clock.add_option('type', default='auto', from_string_converter=clock_by_type)
+
+    _input = None
+
+    def _create_component(self, config=None):
+        reference_clock = self.config.clock.type(config, None)
+        self.component = processing_node.SimpleConsumer(
+            node_id=self.config.id,
+            reference_clock=reference_clock.component
+        )
+
     def __init__(self, config, local_config):
         super(SimpleConsumer, self).__init__(
             config=config,
@@ -56,19 +91,13 @@ class SimpleConsumer(NodeBase):
         self._create_input(config)
 
 
-class ReSequencer(SimpleConsumer):
+class ReSequencer(ProducerMixin, ConsumerMixin, NodeBase):
 
     required_config = Namespace()
     required_config.add_option('sequence_identifier', default='ReSequenced1')
     required_config.add_option('segment_length', default=2.0)
     required_config.add_option('utc', default=False)
     required_config.add_option('discard', default=True)
-    required_config.output = Namespace()
-    required_config.output.carriage = Namespace()
-    required_config.output.carriage.add_option(
-        'type', default='websocket', from_string_converter=producer_carriage_by_type
-    )
-    required_config.output.add_option('adapters')
 
     _output = None
 
@@ -86,24 +115,13 @@ class ReSequencer(SimpleConsumer):
             sequence_identifier=self.config.sequence_identifier
         )
 
-    def _create_output(self, config=None):
-        self._output = self.config.output
-        self._output.carriage = self.config.output.carriage.type.configure_component(
-            config, self.config.output.carriage)
-
-        self._output.adapters = ProducerNodeCarriageAdapter.configure_component(
-            config=config,
-            local_config=self.config.output.adapters,
-            producer=self.component,
-            carriage=self._output.carriage.component
-        )
-
     def __init__(self, config, local_config):
         super(ReSequencer, self).__init__(
             config=config,
             local_config=local_config
         )
-
+        self._create_component(config)
+        self._create_input(config)
         self._create_output(config)
 
         self.backend.register_component_start(self)
@@ -122,17 +140,11 @@ class RetimingDelay(SimpleConsumer):
     required_config.add_option('delay', default=0.0)
 
 
-class SimpleProducer(NodeBase):
+class SimpleProducer(ProducerMixin, NodeBase):
     required_config = Namespace()
     required_config.add_option('id', default='simple-producer')
     required_config.add_option('show_time', default=False)
     required_config.add_option('sequence_identifier', default='TestSequence1')
-    required_config.output = Namespace()
-    required_config.output.carriage = Namespace()
-    required_config.output.carriage.add_option(
-        'type', default='websocket', from_string_converter=producer_carriage_by_type
-    )
-    required_config.output.add_option('adapters')
     required_config.clock = Namespace()
     required_config.clock.add_option('type', default='local', from_string_converter=clock_by_type)
 
@@ -192,6 +204,10 @@ class SimpleProducer(NodeBase):
         self.backend.call_periodically(self.component.resume_producing, interval=2.0)
 
 
+class EBUTTDEncoder(NodeBase):
+    pass
+
+
 def nodes_by_type(node_name):
     if node_name == 'simple-consumer':
         return SimpleConsumer
@@ -199,6 +215,8 @@ def nodes_by_type(node_name):
         return SimpleProducer
     elif node_name == 'resequencer':
         return ReSequencer
+    elif node_name == 'ebuttd-encoder':
+        return EBUTTDEncoder
     else:
         raise ConfigurationError(ERR_CONF_NO_SUCH_NODE.format(
             node_type=node_name
