bd7ddc01ecead791d9033db57597983ad2011b6e
Zoltan Kozma <zoltan.kozma.ext@bbc.co.uk>
2017-03-06 10:35:24 +0000

    #368 First configurator implementation for handover node

--- ebu_tt_live/config/node.py
+++ ebu_tt_live/config/node.py
@@ -294,6 +294,22 @@ class Distributor(ConsumerMixin, ProducerMixin, NodeBase):
         self._create_output(config)
 
 
+class Handover(ConsumerMixin, ProducerMixin, NodeBase):
+
+    def _create_component(self, config):
+        self.component = processing_node.HandoverNode(
+            node_id=self.config.node_id,
+            authors_group_identifier=self.config.authors_group_identifier,
+            sequence_identifier=self.config.sequence_identifier
+        )
+
+    def __init__(self, config, local_config):
+        super(Handover, self).__init__(config, local_config)
+        self._create_component(config)
+        self._create_input(config)
+        self._create_output(config)
+
+
 nodes_by_type = {
     'simple-consumer': SimpleConsumer,
     'simple-producer': SimpleProducer,
@@ -301,7 +317,8 @@ nodes_by_type = {
     'ebuttd-encoder': EBUTTDEncoder,
     'buffer-delay': BufferDelay,
     'retiming-delay': RetimingDelay,
-    'distributor': Distributor
+    'distributor': Distributor,
+    'handover': Handover
 }
 
 
