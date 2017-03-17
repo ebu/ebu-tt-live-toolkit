f2f51872c8ac24e838a76b79e5e29a945aa4cea7
Zoltan Kozma <zoltan.kozma.ext@bbc.co.uk>
2017-03-17 16:38:55 +0000

    comments on tests, js reflow and auto indent.

--- ebu_tt_live/node/test/test_handover_unit.py
+++ ebu_tt_live/node/test/test_handover_unit.py
@@ -47,18 +47,19 @@ class HandoverUnittests(TestCase):
         self.handover.process_document(doc1)
         self.handover.process_document(doc2)
 
+        # Nothing is supposed to be processed when documents lack identifiers
         self.handover.producer_carriage.emit_data.assert_not_called()
 
     def test_switch_and_emit(self):
         doc1 = self._create_test_document(
             sequence_identifier='seq1',
-            sequence_number=1,
+            sequence_number=4,
             authors_group_identifier=self._authors_group_identifier,
             authors_group_control_token=1
         )
         doc2 = self._create_test_document(
             sequence_identifier='seq2',
-            sequence_number=1,
+            sequence_number=4,
             authors_group_identifier=self._authors_group_identifier,
             authors_group_control_token=2
         )
@@ -66,14 +67,21 @@ class HandoverUnittests(TestCase):
         self.handover.process_document(doc1)
         self.handover.process_document(doc2)
 
-        assert self.handover.producer_carriage.emit_data.call_count, 2
+        assert self.handover.producer_carriage.emit_data.call_count, 2  # 2 documents were processed
+        # First document should be doc1
         assert self.handover.producer_carriage.emit_data.call_args_list[0][1]['data'] == doc1
+        # Second document should be doc2
         assert self.handover.producer_carriage.emit_data.call_args_list[1][1]['data'] == doc2
+        # Sequence reassignment
         assert doc1.sequence_identifier == self._sequence_identifier
+        # Sequence number reassignment
         assert doc1.sequence_number == 1
+        # Same for doc2
         assert doc2.sequence_identifier == self._sequence_identifier
         assert doc2.sequence_number == 2
+        # Check current tracked sequence is seq2
         assert self.handover._current_selected_input_sequence_id == 'seq2'
+        # Last issued sequence number is 2
         assert self.handover._last_sequence_number == 2
 
     def test_kwargs_passthrough(self):
@@ -84,6 +92,8 @@ class HandoverUnittests(TestCase):
             authors_group_control_token=1
         )
 
+        # This checks that arbitrary keyword arguments survive the process_document as it is a requirement
+        # of the INode interface it implements.
         self.handover.process_document(document=doc1, bla=4)
 
         self.handover.producer_carriage.emit_data.assert_called_with(
