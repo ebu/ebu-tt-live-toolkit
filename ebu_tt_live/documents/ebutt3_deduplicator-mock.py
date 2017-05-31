26b1652269ffb22173ac92ee968eaa178daea9e5
Shahnaz Hameed <shahnaz.hameed@bbc.co.uk>
2017-05-31 15:34:57 +0100

    Amendments
    Forgot to remove some stuff that doesn’t work yet.

--- ebu_tt_live/documents/ebutt3_deduplicator-mock.py
+++ ebu_tt_live/documents/ebutt3_deduplicator-mock.py
@@ -2,21 +2,22 @@ from pyxb.binding.basis import NonElementContent, ElementContent
 from pyxb import BIND
 from ebu_tt_live import bindings
 
-original_styles =   [
-                        [style_1 = bindings.style_type( id='SEQ58.defaultStyle1',
-                                                        color='rgb(255,255,255)',
-                                                        backgroundColor='rgb(0,0,0)')],
-                        [style_2 = bindings.style_type( id='SEQ59.defaultStyle1',
-                                                        color='rgb(255,255,255)',
-                                                        backgroundColor='rgb(0,0,0)')],
-#                        [style_3 = bindings.style_type( id= 'SEQ60.defaultStyle1',
-#                                                        color= 'rgb(0,255,255)',
-#                                                        backgroundColor= 'rgb(0,0,0)')]
-                    ]
+style_1 = bindings.style_type( id='SEQ58.defaultStyle1',
+                                color='rgb(255,255,255)',
+                                backgroundColor='rgb(0,0,0)')
+
+style_2 = bindings.style_type( id='SEQ59.defaultStyle1',
+                                color='rgb(255,255,255)',
+                                backgroundColor='rgb(0,0,0)')
+
+#style_3 = bindings.style_type(  id= 'SEQ60.defaultStyle1',
+#                                color= 'rgb(0,255,255)',
+#                                backgroundColor= 'rgb(0,0,0)')
 
 list_a = []
 list_b = []
-new_style_list = [[],[],[],[]]
+old_style_list = [[],[],[],[]]
+new_style_list = []
 
 list_a.append(style_1.color)
 list_a.append(style_1.backgroundColor)
@@ -25,11 +26,11 @@ list_b.append(style_2.color)
 list_b.append(style_2.backgroundColor)
 
 if list_a == list_b:
-    new_style_list[0].append(style_1.id)
-    new_style_list[1].append(style_1.color)
-    new_style_list[2].append(style_1.backgroundColor)
+    old_style_list[0].append(style_1.id)
+    old_style_list[1].append(style_1.color)
+    old_style_list[2].append(style_1.backgroundColor)
 
 for i in range(len(old_style_list[0])):
-    new_style_list[3].append("style" + str(i))
+    old_style_list[3].append("style" + str(i))
 
-print (new_style_list)
+print (old_style_list)
