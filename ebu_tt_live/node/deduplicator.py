298fca70d234c3e5e4c2b5dbba3705841db43f5a
Shahnaz Hameed <shahnaz.hameed@bbc.co.uk>
2017-08-02 17:39:20 +0100

    Some refactoring of deduplicator
    Now uses only _three_ dictionaries!

--- ebu_tt_live/node/deduplicator.py
+++ ebu_tt_live/node/deduplicator.py
@@ -18,7 +18,8 @@ class DeDuplicatorNode(AbstractCombinedNode):
     _expects = EBUTT3Document
     _provides = EBUTT3Document
 
-    def __init__(self, node_id, sequence_identifier, consumer_carriage=None, producer_carriage=None):
+    def __init__(self, node_id, sequence_identifier, consumer_carriage=None, \
+                 producer_carriage=None):
         super(DeDuplicatorNode, self).__init__(
             node_id=node_id,
             consumer_carriage=consumer_carriage,
@@ -45,71 +46,63 @@ class DeDuplicatorNode(AbstractCombinedNode):
                 self.producer_carriage.emit_data(data=document, **kwargs)
 
     def remove_duplication(self, document):
-        _new_style_set = set()
-        _new_region_set = set()
-        _old_style_id_dict = dict({})
-        _new_style_id_dict = dict({})
-        _old_region_id_dict = dict({})
-        _new_region_id_dict = dict({})
-        hash_style_dict = dict({})
-        new_style_list = list()
-
-        hash_region_dict = dict({})
-        new_region_list = list()
+        _old_id_dict = dict({})
+        _new_id_dict = dict({})
+        hash_dict = dict({})
 
         if document.binding.head.styling is not None and document.binding.head.layout is not None:
             styles = document.binding.head.styling.style
+            document.binding.head.styling.style = None
+
             regions = document.binding.head.layout.region
+            document.binding.head.layout.region = None
 
             for value in styles:
+                #deduplicating style elements
                 if value is not None:
                     unique_val = ComparableStyle(value)
                     # stores references of original <xml:id> to <my_hash>
-                    _old_style_id_dict[value.id] = unique_val.my_hash
+                    _old_id_dict[value.id] = unique_val.my_hash
                     # stores references of <my_hash> to <tt:style>
-                    hash_style_dict[unique_val.my_hash] = value
+                    hash_dict[unique_val.my_hash] = value
 
+            for hash_val in hash_dict:
+                new_id = hash_dict.get(hash_val)
+                for old_s in styles:
+                    if old_s.id is new_id.id:
+                        document.binding.head.styling.style.append(new_id)
 
-            for style_hash in hash_style_dict:
-                new_id = hash_style_dict.get(style_hash)
-                new_style_list.append(new_id)
                 # stores references of <my_hash> to new <xml:id>
-                _new_style_id_dict[style_hash] = new_id.id
+                _new_id_dict[hash_val] = new_id.id
 
             for value in regions:
+                #in-line style removal
                 if value.style is not None:
                     for old_id_index in range(len(value.style)):
-                        old_id_ref = _old_style_id_dict.get(value.style[old_id_index])
-                        new_id_ref = _new_style_id_dict.get(old_id_ref)
+                        old_id_ref = _old_id_dict.get(value.style[old_id_index])
+                        new_id_ref = _new_id_dict.get(old_id_ref)
 
                         value.style[old_id_index] = new_id_ref
 
+                #deduplicating region elements
                 if value is not None:
                     unique_val = ComparableRegion(value)
                     # stores references of original <xml:id> to <my_hash>
-                    _old_region_id_dict[value.id] = unique_val.my_hash
+                    _old_id_dict[value.id] = unique_val.my_hash
                     # stores references of <my_hash> to <tt:region>
-                    hash_region_dict[unique_val.my_hash] = value
+                    hash_dict[unique_val.my_hash] = value
 
-            for region_hash in hash_region_dict:
-                new_id = hash_region_dict.get(region_hash)
-                new_region_list.append(new_id)
-                _new_region_id_dict[region_hash] = new_id.id
+            for hash_val in hash_dict:
+                new_id = hash_dict.get(hash_val)
+                for old_r in regions:
+                    if old_r.id is new_id.id:
+                        document.binding.head.layout.region.append(new_id)
 
-
-            document.binding.head.styling.style = None
-            for new_style in new_style_list:
-                document.binding.head.styling.append(new_style)
-
-            document.binding.head.layout.region = None
-            for new_region in new_region_list:
-                document.binding.head.layout.append(new_region)
+                _new_id_dict[hash_val] = new_id.id
 
             replace_id_refs = ReplaceStylesAndRegions(document.binding, \
-                                                      _old_style_id_dict, \
-                                                      _new_style_id_dict, \
-                                                      _old_region_id_dict, \
-                                                      _new_region_id_dict)
+                                                      _old_id_dict, \
+                                                      _new_id_dict)
             replace_id_refs.proceed()
         else:
             return None
@@ -172,22 +165,18 @@ class ComparableRegion:
     def __hash__(self):
         return self.my_hash
 
-class ReplaceStylesAndRegions(RecursiveOperation):
-    _old_style_id_dict = None
-    _new_style_id_dict = None
-    _old_region_id_dict = None
-    _new_region_id_dict = None
 
-    def __init__(self, root_element, _old_style_id_dict, _new_style_id_dict, \
-                 _old_region_id_dict, _new_region_id_dict):
+class ReplaceStylesAndRegions(RecursiveOperation):
+    _old_id_dict = None
+    _new_id_dict = None
+
+    def __init__(self, root_element, _old_id_dict, _new_id_dict):
             super(ReplaceStylesAndRegions, self).__init__(
                 root_element
             )
 
-            self._old_style_id_dict = _old_style_id_dict
-            self._new_style_id_dict = _new_style_id_dict
-            self._old_region_id_dict = _old_region_id_dict
-            self._new_region_id_dict = _new_region_id_dict
+            self._old_id_dict = _old_id_dict
+            self._new_id_dict = _new_id_dict
 
     def _is_begin_timed(self, value):
         pass
@@ -201,15 +190,13 @@ class ReplaceStylesAndRegions(RecursiveOperation):
     def _process_element(self, value, element=None, parent_binding=None, **kwargs):
         # The latter part of this and the next test is to check that the instance
         # is not a styling or layout element as these can also have style attributes
-        if hasattr(value, 'style') and value.style is not None and not isinstance(value, bindings.styling):
+        if hasattr(value, 'style') and value.style is not None and not \
+        isinstance(value, bindings.styling):
             id_to_index_dict = dict()
-            #print len(value.style)
 
             for old_id_index in range(len(value.style)-1, -1, -1):
-                old_id_ref = self._old_style_id_dict.get(value.style[old_id_index])
-                new_id_ref = self._new_style_id_dict.get(old_id_ref)
-                #print old_id_ref
-                #print new_id_ref
+                old_id_ref = self._old_id_dict.get(value.style[old_id_index])
+                new_id_ref = self._new_id_dict.get(old_id_ref)
 
                 # Next two lines remove in-line style duplication
                 if new_id_ref in id_to_index_dict:
@@ -221,9 +208,10 @@ class ReplaceStylesAndRegions(RecursiveOperation):
         else:
             pass
 
-        if hasattr(value, 'region') and value.region is not None and not isinstance(value, bindings.layout):
-            old_id_ref = self._old_region_id_dict.get(value.region)
-            new_id_ref = self._new_region_id_dict.get(old_id_ref)
+        if hasattr(value, 'region') and value.region is not None and not \
+        isinstance(value, bindings.layout):
+            old_id_ref = self._old_id_dict.get(value.region)
+            new_id_ref = self._new_id_dict.get(old_id_ref)
 
             value.region = new_id_ref
         else:
--- ebu_tt_live/node/test/test_deduplication.py
+++ ebu_tt_live/node/test/test_deduplication.py
@@ -1,17 +1,25 @@
-from unittest import TestCase
+from ebu_tt_live.documents import EBUTT3Document, EBUTTDDocument, EBUTTAuthorsGroupControlRequest
+from ebu_tt_live.node.deduplicator import DeDuplicatorNode
+from ebu_tt_live.carriage.interface import IProducerCarriage
+from ebu_tt_live.errors import UnexpectedSequenceIdentifierError
 from ebu_tt_live import bindings
-from ebu_tt_live.bindings import ebuttdt as datatypes
-from ebu_tt_live.bindings import ebuttm as metadata
-from ebu_tt_live.documents.converters import ebutt3_to_ebuttd
-from ebu_tt_live.documents.ebutt3 import EBUTT3Document
-from ebu_tt_live.bindings import div_type, p_type, span_type, br_type, ebuttdt
+from ebu_tt_live.bindings import _ebuttm as metadata
+from ebu_tt_live.bindings import _ebuttdt as datatypes
+from datetime import timedelta
+from unittest import TestCase
+from pyxb import BIND
+from mock import MagicMock
 
-class TestEBUTT3DocumentDeDuplication(TestCase):
 
-    def test_input_file(self):
-        tt = bindings.tt(
-            sequenceIdentifier='testSequence001',
-            sequenceNumber='1'
+class DeDuplicatorUnitTest(TestCase):
+
+    def _create_test_document(self):
+        doc = EBUTT3Document(
+            time_base='clock',
+            clock_mode='local',
+            lang='en-GB',
+            sequence_identifier='testSequenceDeDuplicator01',
+            sequence_number='1'
         )
         head_elem = bindings.head_type(
             metadata.headMetadata_type(
@@ -110,88 +118,29 @@ class TestEBUTT3DocumentDeDuplication(TestCase):
             )
         )
 
-        tt.head = head_elem
-        tt.body = body_elem
+        doc.binding.head = head_elem
+        doc.binding.body = body_elem
+        doc.validate()
+        raw_xml = doc.get_xml()
+        doc = EBUTT3Document.create_from_xml(raw_xml)
+        return doc
 
-        document = EBUTT3Document.create_from_raw_binding(binding=tt)
-        document.validate()
+    def setUp(self):
+        carriage = MagicMock(spec=IProducerCarriage)
+        carriage.expects.return_value = EBUTT3Document
 
-    def test_deduplicated_file(self):
-        tt = bindings.tt(
-            sequenceIdentifier='testSequence001',
-            sequenceNumber='1'
+        self.deduplicator = DeDuplicatorNode(
+            node_id='testDeDuplicatorNode',
+            sequence_identifier='testDeDuplicated1',
+            producer_carriage=carriage
         )
-        head_elem = bindings.head_type(
-            metadata.headMetadata_type(
-                metadata.documentMetadata()
-            ),
-            bindings.styling(
-                bindings.style_type(
-                    id='defaultStyle1',
-                    linePadding='0.5c',
-                    backgroundColor='rgb(0, 0, 0)',
-                    color='rgb(255, 255, 255)',
-                    fontFamily='sansSerif'
-                )
-            ),
-            bindings.layout(
-                bindings.region_type(
-                    id='bottomRegion',
-                    displayAlign='after',
-                    extent='71.25% 24%',
-                    origin='14.375% 60%',
-                    overflow='visible',
-                    writingMode='lrtb'
-                )
-            )
-        )
-        body_elem = bindings.body_type(
-            bindings.div_type(
-                bindings.p_type(
-                    bindings.span_type(
-                        'Multiply your anger by about a',
-                        style=['defaultStyle1']
-                    ),
-                    bindings.br_type(),
-                    bindings.span_type(
-                        'hundred, Kate, that\'s how much he',
-                        style=['defaultStyle1']
-                    ),
-                    id='SEQ58.ID001.1'
-                ),
-                region='bottomRegion'
-            ),
-            bindings.div_type(
-                bindings.p_type(
-                    bindings.span_type(
-                        'thinks he loves you.',
-                        style=['defaultStyle1']
-                    ),
-                    id='SEQ59.ID001.1'
-                ),
-                region='bottomRegion'
-            ),
-            bindings.div_type(
-                bindings.p_type(
-                    bindings.span_type(
-                        'Multiply your anger by about a',
-                        style=['defaultStyle1']
-                    ),
-                    bindings.br_type(),
-                    bindings.span_type(
-                        'hundred, Kate, that\'s how much he',
-                        style=['defaultStyle1']
-                    ),
-                    id='SEQ60.ID001.1'
-                ),
-                region='bottomRegion'
-            )
-        )
-        tt.head = head_elem
-        tt.body = body_elem
 
-        document = EBUTT3Document.create_from_raw_binding(binding=tt)
-        document.validate()
+    def test_basic_operation(self):
+        doc = self._create_test_document()
 
-        self.assertIsInstance(document.get_element_by_id('bottomRegion'), bindings.region_type)
-        self.assertIsInstance(document.get_element_by_id('defaultStyle1'), bindings.style_type)
+        self.deduplicator.process_document(document=doc)
+        self.deduplicator.producer_carriage.emit_data.assert_called_once()
+        self.assertIsInstance(
+            self.encoder.producer_carriage.emit_data.call_args[1]['data'],
+            EBUTT3Document
+        )
